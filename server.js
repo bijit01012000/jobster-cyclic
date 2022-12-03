b3fcb91faebc1a609845c401d558b9dde6f3d6bf
bijit <bijitsahu225@gmail.com>
2022-12-04 01:52:57 +0530

    added build

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ if (process.env.NODE_ENV !== "production") {
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // only when ready to deploy
-app.use(express.static(path.resolve(__dirname, './client/build')))
+app.use(express.static(path.join(__dirname, './client/build')))
 
 app.use(express.json());
 app.use(helmet());
@@ -45,7 +45,7 @@ app.use("/api/v1/jobs", authenticateUser, jobsRouter);
 
 // only when ready to deploy
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './client/build', 'index.html'))
+  res.sendFile(path.join(__dirname, './client/build', 'index.html'))
 })
 
 app.use(notFoundMiddleware);
